290d628241aaf3889801b1a7ec887d394c7c561a
anderflu <anderflu@stud.ntnu.no>
2024-03-05 11:43:19 +0100

    made code run

--- hifi-gan-bwe/Testing/test.py
+++ hifi-gan-bwe/Testing/test.py
@@ -1,29 +1,3 @@
-import audioread
-import numpy as np
-import torch
-from IPython.display import Audio
-from hifi_gan_bwe import BandwidthExtender
+import os
 
-#Load the pretrained model
-model = BandwidthExtender.from_pretrained("hifi-gan-bwe-10-42890e3-vctk-48kHz")
-
-#File to extend
-path = 'test.wav'
-
-with audioread.audio_open(path) as input_:
-    sample_rate = input_.samplerate
-    x = (
-        np.hstack([np.frombuffer(b, dtype=np.int16) for b in input_])
-        .reshape([-1, input_.channels])
-        .astype(np.float32)
-        / 32767.0
-    )
-
-Audio(x.T, rate=sample_rate, autoplay=False)
-
-with torch.no_grad():
-    y = np.stack([model(torch.from_numpy(x), sample_rate) for x in x.T]).T
-
-
-#Run the model
-Audio(y.T, rate=int(model.sample_rate), autoplay=False)
+print(os.path.exists("hifi-gan-bwe\Testing/test.wav"))
